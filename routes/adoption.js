fa4582587b81a5eb458aeda799215a3b1fb94b8c
fernandosolis448 <luis.solis.is@unipolidgo.edu.mx>
2022-03-23 17:55:23 -0600

    Update adoption.js

--- routes/adoption.js
+++ routes/adoption.js
@@ -5,7 +5,6 @@ const adoptionValidator = require("../validations/adoptionValidator");
 router.get('/adoption', adoptionValidator.id, adoptionsController.getAdoption);
 router.get('/adoptions', adoptionsController.getAdoptions);
 router.post('/adoption', adoptionValidator.add, adoptionsController.postAdoption);
-router.post('/login', adoptionValidator.id, adoptionsController.getLogin);
 router.put('/adoption', adoptionValidator.update, adoptionsController.putAdoption);
 router.delete('/adoption', adoptionValidator.id, adoptionsController.deleteAdoption);
 router.get('/adoptionUser', adoptionValidator.id, adoptionsController.getAdoptionByUser);
