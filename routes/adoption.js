c2ccf634be6e3ecd07fcced7351f431a7015dc05
fernandosolis448 <luis.solis.is@unipolidgo.edu.mx>
2022-03-28 20:25:14 -0600

    Update adoption.js

--- routes/adoption.js
+++ routes/adoption.js
@@ -2,11 +2,11 @@ const express = require("express");
 const router = express.Router();
 const adoptionsController = require('../controllers/adoptionsController');
 const adoptionValidator = require("../validations/adoptionValidator");
-router.get('/adoption', adoptionValidator.id, adoptionsController.getAdoption);
+router.get('/adoption', adoptionsController.getAdoption);
 router.get('/adoptions', adoptionsController.getAdoptions);
-router.post('/adoption', adoptionValidator.add, adoptionsController.postAdoption);
-router.put('/adoption', adoptionValidator.update, adoptionsController.putAdoption);
-router.delete('/adoption', adoptionValidator.id, adoptionsController.deleteAdoption);
-router.get('/adoptionUser', adoptionValidator.id, adoptionsController.getAdoptionByUser);
+router.post('/adoption', adoptionValidator.add ,adoptionsController.postAdoption);
+router.put('/adoption' , adoptionValidator.update ,adoptionsController.putAdoption);
+router.delete('/adoption', adoptionValidator.id,adoptionsController.deleteAdoption);
+router.get('/adoptionUser', adoptionsController.getAdoptionByUser);
 
 module.exports = router;
