f4315ceda57bdcd2ebc78fceecc3c65b54f8b711
fernandosolis448 <luis.solis.is@unipolidgo.edu.mx>
2022-03-28 20:25:33 -0600

    Update petValidator.js

--- validations/petValidator.js
+++ validations/petValidator.js
@@ -1,50 +1,55 @@
-const { check, validationResult } = require('express-validator');
+const{check, validationResult } = require("express-validator");
+const { is } = require("express/lib/request");
 
-const generatePetValidators = () => [
-   check('alias').notEmpty().isLength({max:150}).withMessage("Invalid alias"),
-   check('type').notEmpty().isIn(['DOG','CAT']).withMessage("Invalid type"),
-   check('color').notEmpty().isLength({min:20, max:20}).withMessage("Invalid color"),
-   check('notes').notEmpty().isLength({max:150}).withMessage("Invalid notes")
+const generatePetValidator = () =>[
+
+    check("alias").notEmpty().isLength({max:50}).withMessage("Invalid alias"),
+    check("type").notEmpty().isIn(["DOG","CAT"]).withMessage("Invalid pet is dog o cat"),
+    check("color").notEmpty().isLength({max:50}).withMessage("Invalid color "),
+    check("notes").notEmpty().isLength({max:50}).withMessage("Invalid notes"),
 ]
 
-
-const generateIdValidators = () => [
-    check('id').notEmpty().isNumeric().withMessage("Invalid id"),
+const generateIdPetValidator = () => [
+    check("id").notEmpty().isNumeric().withMessage("ID invalid not found")
 ]
-const updatePetValidators = () => [
-    check('id').notEmpty().isNumeric().withMessage("Invalid id"),
-    check('alias').isLength({max:150}).withMessage("Invalid alias"),
-    check('type').isIn(['DOG','CAT']).withMessage("Invalid type"),
-    check('color').isLength({min:3, max:20}).withMessage("Invalid color"),
-    check('notes').isLength({max:150}).withMessage("Invalid notes")
- ]
 
-const reporter = (req, res, next) => {
+const updatePetValidator = () => [
+    check("id").notEmpty().isNumeric().withMessage("ID invalid not found"),
+    check("alias").notEmpty().isLength({max:50}).withMessage("Invalid alias"),
+    check("type").notEmpty().isIn(["DOG","CAT"]).withMessage("Invalid pet is dog o cat"),
+    check("color").notEmpty().isLength({max:50}).withMessage("Invalid color "),
+    check("notes").notEmpty().isLength({max:50}).withMessage("Invalid notes"),
+]
+
+const reporter = ( req , res , next )=>{
     const errors = validationResult(req);
     if(!errors.isEmpty()){
         return res.status(404).json({
-            "success" : false,
-            "code" : 404,
-            "message" : errors,
-            "data" : []
-
-        });
+            "sucess":false,
+            "code":404,
+            "message":errors,
+            "data": []
+        })
     }
-  next();  
-}
+        next();
+    }
 
-module.exports = {
-    add: [
-        generatePetValidators(),
-        reporter
-    ],
-    id: [
-        generateIdValidators(),
-        reporter
-    ],
-    update:
-    [
-        updatePetValidators(),
-        reporter
-    ]
-};
+    module.exports = {
+
+        add:[
+            generatePetValidator,
+            reporter
+        ],
+        
+        id:[
+            generateIdPetValidator,
+            reporter
+        ],
+
+        update:[
+            updatePetValidator,
+            reporter
+        ]
+
+
+    }
