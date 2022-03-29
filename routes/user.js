eb96c770a712c962c6c6b433406bd07073eec79c
fernandosolis448 <luis.solis.is@unipolidgo.edu.mx>
2022-03-28 20:25:19 -0600

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -1,14 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const usersController = require('../controllers/usersController');
-const userValidator = require('../validations/userValidator');
-const jwtToken = require("../validations/jwtValidation");
-
-router.get('/user', userValidator.id, usersController.getUser);
-router.get('/users', jwtToken.validateToken, usersController.getUsers);
-router.post('/user', userValidator.add, usersController.postUser);
-router.post('/login', userValidator.id, usersController.getLogin);
-router.put('/user', userValidator.update, usersController.putUser);
-router.delete('/user', userValidator.id, usersController.deleteUser);
+const userValidator = require("../validations/userValidator");
+const jwToken = require("../validations/jwtValidation");
+router.get('/user', usersController.getUser);
+router.get('/users', jwToken.validateToken ,usersController.getUsers);
+router.post('/user', userValidator.add,usersController.postUser);
+router.post("/login",userValidator.id,usersController.getLogin);
+router.put('/user', userValidator.update,usersController.putUser);
+router.delete('/user', userValidator.id,usersController.deleteUser);
 
 module.exports = router;
