08671717835377474de0cfa16a0f2e61dacbfb40
fernandosolis448 <luis.solis.is@unipolidgo.edu.mx>
2022-03-28 20:25:28 -0600

    Update jwtValidation.js

--- validations/jwtValidation.js
+++ validations/jwtValidation.js
@@ -1,39 +1,34 @@
 const jwt = require("jsonwebtoken");
 const db = require("../database").config;
 const middleware = {
-    validateToken: function(req,res,next)
-    {
-        const bearerHeader = req.headers["authorization"];
-        if(typeof bearerHeader !== 'undefined')
-        {
-            const bearer = bearerHeader.split("");
-            const bearerToken =bearer[1];
-            jwt.verify(bearerToken, db.secret_key, (err, data) => {
-                if (err)
-                {
+    validateToken: function(req,res,next){
+        const bearerHeader = req.headers ["authorization"];
+        if(typeof bearerHeader !== "undefined"){
+            const bearer = bearerHeader.split(" ");
+            const bearerToken = bearer[1];
+            jwt.verify(bearerToken,db.secret_key, (err, data)=>{
+                if(err){
                     res.status(401);
                     res.json({
-                        "success" : false,
-                        "code" : 401,
-                        "message" : "invalid token",
-                        "data" : err
-                     });
+                        "success":false,
+                        "code":401,
+                        "message": "Invalid token",
+                        "data":err
+                    });
+
                 }
-                else
-                {
+                else{
                     next()
                 }
             });
-        }
-        else
-        {
-          res.status(401);
-          res.json({
-            "success" : false,
-            "code" : 401,
-            "message" : "You need a token",
-            "data" : []
-          });
+        }else{
+            res.status(401);
+            res.json({
+                "sucees": false,
+                "code":401,
+                "message":"You need a token",
+                "data":[]
+            })
         }
     }
 }
